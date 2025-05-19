dab788c853bb258d18b68298f7abc5fc6832f173
dongcheolpark <head02325@gmail.com>
2025-05-19 18:07:57 +0900

    fix

--- web/app/routes/article.$articleId.tsx
+++ web/app/routes/article.$articleId.tsx
@@ -1,7 +1,8 @@
 import type { MetaFunction } from '@remix-run/cloudflare';
-import { Await, useLoaderData } from '@remix-run/react';
+import { Await, Link, useLoaderData } from '@remix-run/react';
 import { Suspense } from 'react';
 import createLoader from 'app/utils/createLoader';
+import { News } from '@prisma/client';
 
 export const meta: MetaFunction = () => {
   return [{ title: 'Article' }, { name: 'description', content: 'Welcome to Remix!' }];
@@ -21,21 +22,34 @@ export default function Show() {
   const { data } = useLoaderData<typeof loader>();
 
   return (
-    <div className="flex h-screen items-center justify-center p-20">
+    <div className="flex h-screen p-20 max-w-4xl mx-auto">
       <Suspense fallback={<div>Loading...</div>}>
-        <Await resolve={data}>
-          {(news) =>
-            news ? (
-              <div>
-                <h1>{news.title}</h1>
-                <p>{news.content}</p>
-              </div>
-            ) : (
-              <div>Article not found</div>
-            )
-          }
-        </Await>
+        <Await resolve={data}>{(news) => (news ? <NewsWrapper news={news} /> : <div>Article not found</div>)}</Await>
       </Suspense>
     </div>
   );
 }
+
+const NewsWrapper = ({ news }: { news: News }) => {
+  return (
+    <div>
+      <Link to="/article" className="text-blue-500 hover:underline">
+        {'⬅️ 리스트로'}
+      </Link>
+      <h1 className="text-3xl font-bold">{news.title}</h1>
+      <div className="flex items-center gap-2 mt-1">
+        <p className="text-gray-500 text-sm">
+          {news.createdAt.toLocaleDateString('ko-KR', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+          })}
+        </p>
+        <a href={news.url} target="_blank" rel="noopener noreferrer" className="text-blue-500 text-sm hover:underline">
+          Read original
+        </a>
+      </div>
+      <p className="mt-4">{news.content}</p>
+    </div>
+  );
+};
