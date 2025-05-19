908ba5f0ba5f642963eed0a368d2ce254fff7932
dongcheolpark <head02325@gmail.com>
2025-05-19 11:35:58 +0900

    a

--- web/app/routes/_index.tsx
+++ web/app/routes/_index.tsx
@@ -1,31 +1,39 @@
 import type { MetaFunction } from '@remix-run/cloudflare';
-import { Link, useLoaderData } from '@remix-run/react';
+import { Await, Link, useLoaderData } from '@remix-run/react';
 import { getSummary } from 'app/API/AI/AI';
+import createLoader from 'app/utils/createLoader';
+import { Suspense } from 'react';
 
 export const meta: MetaFunction = () => {
   return [{ title: 'New Remix App' }, { name: 'description', content: 'Welcome to Remix!' }];
 };
 
-export const loader = async () => {
-  const summary = await getSummary().catch((error) => {
+export const loader = createLoader(async ({ db }) => {
+  const summary = getSummary().catch((error) => {
     console.error('Error while fetching summary:', error);
     return 'Error while fetching summary';
   });
 
+  const test = await db.test.findFirst();
+
   return {
     summary,
+    test,
   };
-};
+});
 
 export default function Index() {
-  const { summary } = useLoaderData<typeof loader>();
+  const { summary, test } = useLoaderData<typeof loader>();
 
   return (
     <div className="flex h-screen items-center justify-center">
       <Link to="/article" className="text-blue-500 hover:underline">
         기사 리스트
       </Link>
-      <div>{summary}</div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Await resolve={summary}>{(summary) => <p>{summary}</p>}</Await>
+      </Suspense>
+      <p>{test?.name}</p>
     </div>
   );
 }
