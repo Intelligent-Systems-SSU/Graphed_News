2cb16ed503b312e578c2cd08fc8f29a17fd568ca
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2025-05-19 17:03:58 +0900

    fix article to db data (#30)

--- web/app/routes/article.$articleId.tsx
+++ web/app/routes/article.$articleId.tsx
@@ -1,50 +1,41 @@
-import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/cloudflare';
+import type { MetaFunction } from '@remix-run/cloudflare';
 import { Await, useLoaderData } from '@remix-run/react';
 import { Suspense } from 'react';
-import { newsApi } from 'app/API/NewsApi/newsApi';
+import createLoader from 'app/utils/createLoader';
 
 export const meta: MetaFunction = () => {
   return [{ title: 'Article' }, { name: 'description', content: 'Welcome to Remix!' }];
 };
 
+export const loader = createLoader(async ({ params, db }) => {
+  if (!params.articleId) {
+    throw new Error('Article ID is required');
+  }
+
+  const data = await db.news.findUnique({ where: { id: Number(params.articleId) } });
+
+  return { data };
+});
+
 export default function Show() {
-  const { promiseNews, promiseBackground } = useLoaderData<typeof loader>();
+  const { data } = useLoaderData<typeof loader>();
 
   return (
     <div className="flex h-screen items-center justify-center p-20">
       <Suspense fallback={<div>Loading...</div>}>
-        <Await resolve={promiseNews}>
-          {(news) => (
-            <div>
-              <h1>{news.title}</h1>
-              <p>{news.content}</p>
-            </div>
-          )}
-        </Await>
-      </Suspense>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Await resolve={promiseBackground}>
-          {(background) => (
-            <div>
-              <h2>Background</h2>
-              {background.map((bg) => (
-                <div key={bg.id}>
-                  <p>{bg.content}</p>
-                </div>
-              ))}
-            </div>
-          )}
+        <Await resolve={data}>
+          {(news) =>
+            news ? (
+              <div>
+                <h1>{news.title}</h1>
+                <p>{news.content}</p>
+              </div>
+            ) : (
+              <div>Article not found</div>
+            )
+          }
         </Await>
       </Suspense>
     </div>
   );
 }
-
-export const loader = async ({ params }: LoaderFunctionArgs) => {
-  if (!params.articleId) {
-    throw new Error('Article ID is required');
-  }
-  const promiseNews = newsApi.getNewsById(params.articleId);
-  const promiseBackground = newsApi.getNewsBackground(params.articleId);
-  return { promiseNews, promiseBackground };
-};
