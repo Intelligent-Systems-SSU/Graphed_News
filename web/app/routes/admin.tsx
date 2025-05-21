2907070f13a27209d416dd7e21d7d8c25fe78431
dongcheolpark <head02325@gmail.com>
2025-05-21 14:20:46 +0900

    a

--- web/app/routes/admin.tsx
+++ web/app/routes/admin.tsx
@@ -7,9 +7,15 @@ export const meta: MetaFunction = () => {
   return [{ title: 'New Remix App' }, { name: 'description', content: 'Welcome to Remix!' }];
 };
 
-export const action = createAction(async ({ request }) => {
-  const url = (await request.formData()).get('url') as string;
-  const success = await getSummary(url)
+export const action = createAction(async ({ request, db }) => {
+  const newsId = (await request.formData()).get('newsId') as string;
+  const news = await db.news.findUnique({ where: { id: Number(newsId) } });
+
+  if (!news) {
+    throw new Error('News not found');
+  }
+
+  const success = await getSummary(Number(newsId), news.url)
     .then(() => true)
     .catch(() => false);
   return { success };
@@ -22,7 +28,7 @@ export default function Index() {
     <div className="flex flex-col max-w-4xl mx-auto px-4 pt-5">
       <h1 className="text-3xl font-bold text-gray-900 mb-8">테스트 페이지</h1>
       <form method="post" className="flex gap-2">
-        <input type="text" name="url" className="border border-gray-300 rounded px-2 py-1" />
+        <input type="text" name="newsId" className="border border-gray-300 rounded px-2 py-1" />
         <button className="bg-blue-500 text-white px-4 py-2 rounded" type="submit">
           테스트
         </button>
