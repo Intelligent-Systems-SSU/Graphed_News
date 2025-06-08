df764b132ae88a8fea47aab97a4aeb19aec3b423
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2025-06-08 13:12:38 +0900

    a (#86)

--- web/app/components/Layout.tsx
+++ web/app/components/Layout.tsx
@@ -1,15 +1,18 @@
 import { ReactNode } from 'react';
-import Navigation from './Navigation';
+import Navigation, { NavContextProv, useNavState } from './Navigation';
 
 type LayoutProps = {
   children: ReactNode;
 };
 
 const Layout = ({ children }: LayoutProps) => {
+  const navState = useNavState();
   return (
     <div className="min-h-screen bg-white flex flex-col">
-      <Navigation />
-      <main className="grow-1 flex flex-col">{children}</main>
+      {navState.isEnable && <Navigation />}
+      <NavContextProv value={navState}>
+        <main className="grow-1 flex flex-col">{children}</main>
+      </NavContextProv>
     </div>
   );
 };
